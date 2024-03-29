9bc876aba0861b4c148aca3129dd90657ba2de5d
huseyin.kaya <huseyin.kaya@interprobe.com.tr>
2024-03-30 01:39:45 +0300

    struct ve enum üzerine çalışmalar yapılıyor...

--- main.c
+++ main.c
@@ -1,6 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <postgresql/libpq-fe.h>
+#include <netinet/in.h>
+
+typedef enum serverInfoGroupEnum{
+    rocky_linux,
+    ubuntu,
+    windows
+}ServerInfoGroupEnum;
+
+typedef struct serverInfoStruct
+{
+    char serverName[30];
+    struct in_addr ip_address; 
+}ServerInfoStruct;
+
+
+typedef struct ip
+{
+    
+}Ip;
+
+ 
+
+
+
+
 
 int main(){
-    
+
+    ServerInfoGroupEnum huseyin = windows;
+    printf("%d", huseyin );
+
+
+}
+
+void show_server_info(ServerInfoStruct s){
+
 }
