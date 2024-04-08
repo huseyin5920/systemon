838a90d5e0c2041c4a5c8de5aa02e6ce70c7db94
huseyin.kaya <huseyin.kaya@interprobe.com.tr>
2024-04-08 03:20:08 +0300

    exit yapısı incelendi

--- main.c
+++ main.c
@@ -4,30 +4,37 @@
 #include <netinet/in.h>
 #include "enums/enums.h"
 #include "structs/structs.h"
+#include<unistd.h>
 
 ServerInfoGroupEnum serverInfoGroupEnum = rocky_linux;
 ServerInfoStruct serverInfoStruct;
-void close_connection(PGconn *connection, int code) {
-  PQfinish(connection);
-  exit(code);
-}
-
 
 
 int main(){
-
-    PGconn *conn = PQconnectdb("user=username password=password dbname=school host=localhost port=5432");
+    //TODO: kritik değişkenler .env dosyasından alınacak.
+    PGconn *conn = PQconnectdb("user=usernsame password=password dbname=systemon host=localhost port=5432");
 
     if (PQstatus(conn) == CONNECTION_BAD) {
         fprintf(stderr, "Connection to database failed %s\n",
                 PQerrorMessage(conn));
-        close_connection(conn, 1);
+        PQfinish(conn);
+        exit(EXIT_FAILURE);
     }  
     if (PQstatus(conn) == CONNECTION_OK) {
         fprintf(stderr, "Connection to database okey %s\n",
                 PQerrorMessage(conn));
-        close_connection(conn, 1);
     }      
+
+    PGresult *res = PQexec(conn, "DROP TABLE IF EXISTS school");
+    
+    if (PQresultStatus(res) != PGRES_COMMAND_OK) {
+        PQclear(res);
+        
+    }
+    
+
+    PQfinish(conn);
+
     // int islem;
 
     // printf("Yapmak istediğiniz işlemi seçiniz: \n1) Sunucu ekleme...\n2) Sunucu listeleme\n");
